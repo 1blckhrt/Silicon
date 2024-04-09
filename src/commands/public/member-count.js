9aa5d2ce8570ad820c55f85b66a692ce3e10a18c
1blckhrt <williams.1691@wright.edu>
2024-04-09 17:37:23 -0400

    added member count, added logger to startup script

---
+++ src/commands/public/member-count.js
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import ms from "ms";
+import logger from "../../util/logger.js";
+import errorEmbed from "../../components/embeds/error.js";
+
+export default {
+  developer: false,
+  cooldown: ms("5s"),
+  data: new SlashCommandBuilder()
+    .setName("member-count")
+    .setDescription("Displays the member count of the server.")
+    .setDMPermission(false),
+  async execute(interaction, client) {
+    try {
+      const icon = client.user.displayAvatarURL();
+      const guild = interaction.guild;
+
+      const embed = new EmbedBuilder()
+        .setTitle(":1234: Server Member Count")
+        .setDescription(`The server has ${guild.memberCount} members.`)
+        .setColor("Green")
+        .setThumbnail(guild.iconURL())
+        .setTimestamp()
+        .setFooter({
+          text: `Requested by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL(),
+        });
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      logger.error(error);
+      const embed = errorEmbed(client, interaction, error);
+      await interaction.reply({ embeds: [embed] });
+    }
+  },
+};
--- src/core/system/handlers/start.js
+++ src/core/system/handlers/start.js
@@ -3,6 +3,7 @@ import registerSlashCommand from "./register-commands.js";
 import loadEvents from "./load-events.js";
 import loadComponents from "./load-components.js";
 import UserEvent from "../../functions/startup-log.js";
+import logger from "../../../util/logger.js";
 
 async function start() {
   try {
@@ -10,11 +11,12 @@ async function start() {
     await registerSlashCommand(client);
     await loadEvents(client);
     await loadComponents(client);
-    
+
     const userEvent = new UserEvent({ client });
     await userEvent.run();
+    logger.success(` Logged in as ${client.user.tag}!`);
   } catch (err) {
-    console.error(err);
+    logger.error(`FATAL ERROR: ${err.message}`);
   }
 }
 
